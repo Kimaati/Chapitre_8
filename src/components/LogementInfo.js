79e692cd701ac4a91b05537f919f11843de8d0fd
Cédric <cedric.lavergn04@hotmail.fr>
2023-09-27 17:51:05 +0200

    Ajout du responsiv V.1

--- src/components/LogementInfo.js
+++ src/components/LogementInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import annonce from "../data/annonce.json";
+import Tag from "../components/Tag";
+import Rating from "../components/Rating";
 
 const LogementInfo = () => {
   const { id } = useParams();
@@ -17,6 +19,16 @@ const LogementInfo = () => {
       <div className="host">
         <p className="name">{logement.host.name}</p>
         <img src={logement.host.picture} alt={logement.host.name} />
+      </div>
+      <div className="tags-rating-container">
+        <div className="tags-container">
+          {logement.tags.map((tag, index) => (
+            <Tag key={index} tag={tag} />
+          ))}
+        </div>
+      </div>
+      <div className="rating-container">
+        <Rating rating={logement.rating} />
       </div>
     </div>
   );
--- src/pages/LogementDetail.js
+++ src/pages/LogementDetail.js
@@ -5,15 +5,11 @@ import Footer from "../components/Footer";
 import Carrousel from "../components/Carrousel";
 import annonce from "../data/annonce.json";
 import LogementInfo from "../components/LogementInfo";
-import Tag from "../components/Tag";
-import Rating from "../components/Rating";
 import DescriptionAccordeon from "../components/DescriptionAccordeon";
 import EquipementAccordeon from "../components/EquipementAccordeon";
-// import styles from "../styles/components/accordeonLogement.scss";
 
 const LogementDetail = () => {
   const { id } = useParams();
-  // console.log("ID de logement dans l'URL :", id);
 
   const logement = annonce.find((item) => item.id === id);
 
@@ -22,16 +18,6 @@ const LogementDetail = () => {
       <Navigation />
       <Carrousel images={logement.pictures} />
       <LogementInfo />
-      <div className="tags-rating-container">
-        <div className="tags-container">
-          {logement.tags.map((tag, index) => (
-            <Tag key={index} tag={tag} />
-          ))}
-        </div>
-        <div className="rating-container">
-          <Rating rating={logement.rating} />
-        </div>
-      </div>
       <div className="accordeon-logement-container">
         <DescriptionAccordeon description={logement.description} />
         <EquipementAccordeon equipments={logement.equipments} />
